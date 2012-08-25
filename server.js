5654d0e9491d5a065bf1d56ed0e4582b93e169a4
Tom Wilson <tom@jackhq.com>
2012-08-25 17:46:33 -0400

    update foo

--- server.js
+++ server.js
@@ -20,24 +20,18 @@ app.http.before = [
 
 app.router.post('/api/deals', function(){
   var self = this, loc = this.req.body;
-  //, filters: {source_namespace: { $eq: "groupon"}}
   factual.get('/places/monetize', {geo:{$circle:{$center:[loc.lat,loc.lng], $meters: 20000}}, filters: { source_namespace: { $neq: "grubhub"}}}, function (error, res) {
     self.res.json(res.data);
-    console.log(res.data);
   });  
 });
 
 
 app.router.post('/api/locate', function(){
   var self = this, loc = this.req.body;
-  //, filters: {source_namespace: { $eq: "groupon"}}
   factual.get('/places/geocode', {geo: {$point: [loc.lat, loc.lng]}}, function(err, res){
-    console.log(res.data);
     var values = {locality: res.data[0].locality, region: res.data[0].region, name: loc.name, latitude: loc.lat, longitude: loc.lng};
-    console.log(values);
     factual.get('/places/resolve', {debug: true, values: values}, function (error, res) {
       self.res.json(res.data);
-      console.log(res.data[0]);
     });      
   });
 });
